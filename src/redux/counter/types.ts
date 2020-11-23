439a4f5624ade5a39d1f84205005c28fdc743216
増田 博紀 <kuzrwkd@icloud.com>
2020-11-23 15:47:05 +0900

    型修正

--- src/redux/counter/types.ts
+++ src/redux/counter/types.ts
@@ -1,15 +1,3 @@
 export type State = {
   num: number
 }
-
-export type IncrementAction = {
-  type: string
-  payload: number
-}
-
-export type DecrementAction = {
-  type: string
-  payload: number
-}
-
-export type Actions = IncrementAction | DecrementAction
