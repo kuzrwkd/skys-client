bb67676256ccd1a67567bf01dba2593d406e3347
増田 博紀 <kuzrwkd@icloud.com>
2021-02-07 19:20:30 +0900

    エンドポイント整理

--- src/pages/api/rss/nikkei/economy/index.ts
+++
@@ -1,9 +0,0 @@
-import { RssWorJpFetchController } from '@/interfaces/controllers/api/rssWorJp'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default (_: NextApiRequest, res: NextApiResponse): void => {
-  const controller = new RssWorJpFetchController('nikkei/economy.rdf', [
-    '政治・経済',
-  ])
-  controller.rssFetch().then((rss) => res.json(rss))
-}
