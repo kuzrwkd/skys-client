c91244814c653f8f8f0e841a1763014dea09fa40
Hiroki Masuda <kuzrwkd@icloud.com>
2023-08-10 00:50:03 +0900

    ♻️ organize configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,13 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['@typescript-eslint','import'],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
     'plugin:import/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'next/core-web-vitals',
+    'prettier',
   ],
   root: true,
   env: {
@@ -21,12 +22,6 @@ module.exports = {
   },
   ignorePatterns: ['*.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-namespace': 'off',
-    '@typescript-eslint/ban-types': 'off',
     'import/order': [
       'error',
       {
@@ -40,13 +35,13 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': [
       'error',
       {
-        'allowSingleExtends': true
-      }
-    ]
+        allowSingleExtends: true,
+      },
+    ],
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-};
+}
