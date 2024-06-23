5774148f307b7a1da8061f8023f5f3d7bbd3a8fa
Hiroki Masuda <kuzrwkd@icloud.com>
2024-06-23 23:38:02 +0900

    Add codegen configuration file
    A new file named 'codegen.ts' was added. This file contains the configurations for the code generation, including plugins to use and file locations. The chosen plugins include 'typescript', 'typescript-operations', 'typescript-resolvers', and 'typescript-rtk-query'.

---
+++ codegen.ts
@@ -0,0 +1,28 @@
+import type {CodegenConfig} from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:30000/graphql',
+  documents: 'src/graphql/documents/**/*.graphql',
+  generates: {
+    'src/graphql/codegen/index.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-resolvers',
+        {
+          'typescript-rtk-query': {
+            importBaseApiFrom: 'src/redux/baseApi',
+            importBaseApiAlternateName: 'baseApiWithGraphql',
+            exportHooks: true,
+          },
+        },
+      ],
+    },
+    './graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
+
+export default config;
