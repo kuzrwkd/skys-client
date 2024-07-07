6cd722f0ae977a3a40066944159fb675e27e0d90
Hiroki Masuda <kuzrwkd@icloud.com>
2024-07-07 18:14:20 +0900

    Refine Redux API setup and update eslint ignore
    In the Redux base API, TypeScript typecasting is now included for the `graphqlBaseQuery` to improve type safety. Additionally, an eslint ignore file has been created to exclude the `src/graphql/codegen` folder from linting rules. The `skysApi.ts` file has been updated to include a new export function `useLazyGetNewsfeedQuery`.

--- src/redux/services/skysApi.ts
+++ src/redux/services/skysApi.ts
@@ -9,4 +9,4 @@ export const skysApi = generatedApi.enhanceEndpoints({
   },
 });
 
-export const {useGetNewsfeedQuery} = skysApi;
+export const {useGetNewsfeedQuery, useLazyGetNewsfeedQuery} = skysApi;
