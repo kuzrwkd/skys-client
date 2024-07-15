19237b78822ac684a941533e8d514176574005e5
Hiroki Masuda <kuzrwkd@icloud.com>
2024-07-15 12:04:14 +0900

    Update module configurations and improve typecasting
    Changed the 'module' and 'moduleResolution' property values in both browser and querys' tsconfig.json files. Adjusted from 'esnext' and 'nodenext' to 'ES2022' and 'Bundler' respectively. Also enhanced the typecasting in 'baseApi.ts' within the Querys package to ensure safer data manipulation.

--- packages/querys/src/graphql/baseApi.ts
+++ packages/querys/src/graphql/baseApi.ts
@@ -1,10 +1,11 @@
 import {createApi} from '@reduxjs/toolkit/query/react';
 import {graphqlRequestBaseQuery} from '@rtk-query/graphql-request-base-query';
 import {GraphQLClient} from 'graphql-request';
+import type {BaseQueryFn} from '@reduxjs/toolkit/query';
 
 export const client: any = new GraphQLClient('http://localhost:30000/graphql');
 
-const graphqlBaseQuery = graphqlRequestBaseQuery({client});
+const graphqlBaseQuery = graphqlRequestBaseQuery({client}) as BaseQueryFn;
 
 export const baseApiWithGraphql = createApi({
   baseQuery: graphqlBaseQuery,
