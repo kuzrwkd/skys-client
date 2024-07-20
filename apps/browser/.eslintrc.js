5d99218b4f084ae394706db6a5380ea761d1c1d6
Hiroki Masuda <kuzrwkd@icloud.com>
2024-07-20 18:20:19 +0900

    Fix typo in .eslintrc.js file
    The spelling error in the array of packageDir in .eslintrc.js file has been fixed. Specifically, 'querys' has been corrected to 'queries' to maintain code consistency and avoid potential runtime errors.

--- apps/browser/.eslintrc.js
+++ apps/browser/.eslintrc.js
@@ -62,7 +62,7 @@ module.exports = {
         packageDir: [
           '.',
           '../..',
-          '../../packages/querys',
+          '../../packages/queries',
           '../../packages/skys-base-ui',
         ],
       },
