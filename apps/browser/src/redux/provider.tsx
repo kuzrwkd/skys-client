3ea3fddb1c31ec72b4c7bc782235ac2641e58291
Hiroki Masuda <kuzrwkd@icloud.com>
2024-07-21 02:27:31 +0900

    Refactor store import path in redux files
    The import path for the store is updated in both `provider.tsx` and `hooks.ts` within redux directory. This was required due to the renaming of the file `store.ts` to `index.ts`, which simplifies and standardizes the import process.

--- apps/browser/src/redux/provider.tsx
+++ apps/browser/src/redux/provider.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import {Provider} from 'react-redux';
-import {store} from './store';
+import {store} from '.';
 
 export default function ReduxProvider({children}: {children: React.ReactNode}) {
   return <Provider store={store}>{children}</Provider>;
