6f9141e7807601ff3bfcf08d142df1d25021202c
Hiroki Masuda <kuzrwkd@icloud.com>
2024-08-06 21:47:43 +0900

    Update GraphQL client URL to use environment variable
    Change GraphQL client initialization to use the NEXT_PUBLIC_API_BASE_URL environment variable instead of a hardcoded URL. This makes the application more flexible and easier to configure for different environments.

--- packages/queries/src/graphql/baseApi.ts
+++ packages/queries/src/graphql/baseApi.ts
@@ -3,7 +3,7 @@ import {graphqlRequestBaseQuery} from '@rtk-query/graphql-request-base-query';
 import {GraphQLClient} from 'graphql-request';
 import type {BaseQueryFn} from '@reduxjs/toolkit/query';
 
-export const client: any = new GraphQLClient('http://localhost:30000/graphql');
+export const client: any = new GraphQLClient(process.env.NEXT_PUBLIC_API_BASE_URL!);
 
 const graphqlBaseQuery = graphqlRequestBaseQuery({client}) as BaseQueryFn;
 
